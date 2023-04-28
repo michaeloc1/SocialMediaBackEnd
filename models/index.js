90205f7ed56335145ea892ff6a00814c8120818c
michaeloc1 <michaeloc@verizon.net>
2023-04-28 15:23:19 -0400

    added functionality to create, delete, and get users

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+//const Tag = require('./Tag');
+//const Application = require('./Application');
+
+//module.exports = { User, Application, Tag };
+
+module.exports = { User };
